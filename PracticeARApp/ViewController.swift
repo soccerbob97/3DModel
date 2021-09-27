ac9720b4e9ef7dd6b47bd60472f762ab41174207
hkaranth3 <hkaranth3@gatech.edu>
2021-09-27 14:41:53 -0400

    Initial Commit

---
+++ PracticeARApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PracticeARApp
+//
+//  Created by Harsha Karanth on 9/27/21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
